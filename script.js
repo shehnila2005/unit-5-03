6fdf1ca1e62b665613535a81174c4aa85b06d98d
Shehnila Azmat <shehnila.azmat@ocsbstudent.ca>
2021-02-12 14:08:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('display-message').addEventListener('click', buttonOn)
+
+function buttonOn() {
+  alert ('my name is Shehnila!')
+}
+ function button2() {
+   document.getElementById('paragraph').innerHTML = 'hello world!'
+ document.getElementById('background').style.backgroundcolor = 'blue'
+ }
